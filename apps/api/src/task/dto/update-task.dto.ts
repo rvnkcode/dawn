3110f614aedc179841f12341d5dcf204c58444f3
KSY <sykim.p508@gmail.com>
2023-02-24 13:27:53 +0000

    api: add optional api property

--- apps/api/src/task/dto/update-task.dto.ts
+++ apps/api/src/task/dto/update-task.dto.ts
@@ -1,10 +1,11 @@
 import { CreateTaskDto } from "./create-task.dto";
 import { PartialType } from "@nestjs/mapped-types";
-import { IsBoolean } from "class-validator";
+import { IsBoolean, IsOptional } from "class-validator";
 
 export class UpdateTaskDto extends PartialType(CreateTaskDto) {
   title?: string;
 
+  @IsOptional()
   @IsBoolean()
   isDone?: boolean;
 }
