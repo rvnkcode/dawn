e34128b2974cc7db33137bf918777f22896d2102
KSY <ksy@rvnk.dev>
2025-11-23 15:49:42 +0900

    feat: handle `add` command

--- inbounds/cli/src/lib/cli.rs
+++ inbounds/cli/src/lib/cli.rs
@@ -1,13 +1,25 @@
-use clap::Parser;
+use crate::context::AppContext;
+use crate::handler::Handler;
+use clap::{Args, Parser, Subcommand};
 use dawn::domain::task::port::TaskService;
-
-struct AppContext<TS: TaskService> {
-    task_service: TS,
-}
 
 #[derive(Parser)]
 #[command(about = "A command line todo manager.", long_about = None, subcommand_precedence_over_arg = true, version)]
-pub struct Cli {}
+pub struct Cli {
+    filters: Vec<String>,
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Add(Modification),
+}
+
+#[derive(Args)]
+pub struct Modification {
+    pub description: Vec<String>,
+}
 
 impl Cli {
     pub fn new() -> Self {
@@ -15,7 +27,17 @@ impl Cli {
     }
 
     pub fn handle_command(&self, task_service: impl TaskService) -> anyhow::Result<()> {
-        let _context = AppContext { task_service };
+        let context = AppContext::new(task_service);
+        let handler = Handler::new(context);
+
+        match &self.command {
+            Some(Commands::Add(modification)) => {
+                handler.add(&self.filters, modification);
+            }
+            None => {
+                // TODO: next list
+            }
+        }
         Ok(())
     }
 }
--- inbounds/cli/src/lib/lib.rs
+++ inbounds/cli/src/lib/lib.rs
@@ -1,2 +1,4 @@
 pub mod cli;
 pub use cli::Cli;
+pub mod context;
+pub mod handler;
