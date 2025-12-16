52bcc7eeddde73d200d1b84a2c418e6475c4d2eb
rvnkcode <sykim.p508@gmail.com>
2025-12-16 21:21:37 +0900

    refactor: uses `if let Err()` pattern at `main.rs`

--- inbounds/cli/src/main.rs
+++ inbounds/cli/src/main.rs
@@ -6,11 +6,9 @@ fn main() -> anyhow::Result<()> {
     let db = SQLite::new()?;
     let task_service = task::Service::new(db);
     let cli = Cli::new();
-    match cli.handle_command(task_service) {
-        Ok(_) => Ok(()),
-        Err(e) => {
-            eprintln!("{}", e.to_string().white().on_red());
-            std::process::exit(1);
-        }
+    if let Err(e) = cli.handle_command(task_service) {
+        eprintln!("{}", e.to_string().white().on_red());
+        std::process::exit(1);
     }
+    Ok(())
 }
