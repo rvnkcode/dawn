6d3e3ba7b8ec5c3602445a3a81eb654cbcf65534
KSY <ksy@rvnk.dev>
2026-01-03 13:58:11 +0900

    refactor: introduced `get_status` method to `Status`

--- inbounds/cli/src/lib/handler/status.rs
+++ inbounds/cli/src/lib/handler/status.rs
@@ -1,6 +1,18 @@
 #[derive(Debug, PartialEq)]
+use dawn::domain::task::Task;
+
 pub enum Status {
     Pending,
     Completed,
     Deleted,
+}
+
+impl Status {
+    pub fn get_status(task: &Task) -> Self {
+        match (&task.deleted_at, &task.completed_at) {
+            (Some(_), _) => Status::Deleted,
+            (None, Some(_)) => Status::Completed,
+            (None, None) => Status::Pending,
+        }
+    }
 }
