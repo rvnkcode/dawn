4e33709ff679d80e8c67bc4f4679f8ceda6172b6
KSY <ksy@rvnk.dev>
2026-04-12 16:36:59 +0900

    perf: parse CLI first to prevent unnecessary DB initialization with `--help` or `--version`
    refs: #12

--- src/bin/cli/main.rs
+++ src/bin/cli/main.rs
@@ -1,10 +1,10 @@
 use dawn::{domain::task::service::Service, inbound::Cli, outbound::SQLite};
 
 fn main() -> anyhow::Result<()> {
+    let cli = Cli::new();
     let mut db = SQLite::new()?;
     db.initialize()?;
     let task_service = Service::new(db);
-    let cli = Cli::new();
     cli.handle_command(task_service)?;
     Ok(())
 }
