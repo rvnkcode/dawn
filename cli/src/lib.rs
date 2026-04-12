3cbf16e453889b29508000b2aa3dc633c806de43
KSY <ksy@rvnk.dev>
2026-04-12 22:16:58 +0900

    docs: reason why `CLI` needs separated `lib.rs`
    refs: #18

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,3 +1,9 @@
+/*
+ * This lib target exists so that `cargo llvm-cov --workspace --lib` includes
+ * CLI unit tests in coverage reports. Removing it would cause a compile error
+ * because `SQLite::new()` is `#[cfg(not(coverage))]`.
+ */
+
 mod arg;
 mod cli;
 mod handler;
