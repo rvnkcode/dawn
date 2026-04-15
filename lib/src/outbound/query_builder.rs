e3ecac81b268ffda790ff7e682821deb8d6481aa
KSY <ksy@rvnk.dev>
2026-04-15 16:41:00 +0900

    refactor: `repeat_vars()`

--- lib/src/outbound/query_builder.rs
+++ lib/src/outbound/query_builder.rs
@@ -138,9 +138,7 @@ pub(crate) fn build_update_clause(
 
 // ref: https://docs.rs/rusqlite/latest/rusqlite/struct.ParamsFromIter.html#realistic-use-case
 pub(crate) fn repeat_vars(count: usize) -> String {
-    std::iter::repeat_n("?", count)
-        .collect::<Vec<_>>()
-        .join(",")
+    vec!["?"; count].join(",")
 }
 
 #[cfg(test)]
