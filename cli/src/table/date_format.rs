e3554017d2417f5f565af7beb58f43e573c34f49
KSY <ksy@rvnk.dev>
2026-04-23 10:59:41 +0900

    test: removed timezone dependant tests from `date_format`
    refs: #20

--- cli/src/table/date_format.rs
+++ cli/src/table/date_format.rs
@@ -18,32 +18,9 @@ pub(crate) fn format_absolute(ts: &Timestamp) -> Result<String, AgeError> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use chrono::TimeZone;
 
     fn ts(secs: i64) -> Timestamp {
         Timestamp::new(secs).unwrap()
-    }
-
-    #[test]
-    fn format_absolute_in_renders_local_date() {
-        // 2024-01-15 00:00:00 UTC → 2024-01-15 09:00:00 KST
-        let epoch = chrono::Utc
-            .with_ymd_and_hms(2024, 1, 15, 0, 0, 0)
-            .unwrap()
-            .timestamp();
-        let out = format_absolute(&ts(epoch)).unwrap();
-        assert_eq!(out, "2024-01-15 09:00:00");
-    }
-
-    #[test]
-    fn format_absolute_in_crosses_date_boundary() {
-        // 2024-01-14 23:30:00 UTC → 2024-01-15 08:30:00 KST
-        let epoch = chrono::Utc
-            .with_ymd_and_hms(2024, 1, 14, 23, 30, 0)
-            .unwrap()
-            .timestamp();
-        let out = format_absolute(&ts(epoch)).unwrap();
-        assert_eq!(out, "2024-01-15 08:30:00");
     }
 
     #[test]
